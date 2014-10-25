0260dcd856ae17262a9c459e817388f7117dcd92
adri42 <adrien.moleins@telecom-st-etienne.fr>
2014-10-25 16:26:12 +0200

    renvoie un tableau de string
    return string[] et javadoc ajouté

--- src/monprojinfo/principal.java
+++ src/monprojinfo/principal.java
@@ -7,10 +7,11 @@ public class principal {
 	public static void main(String[] args) throws TwitterException {
 		// TODO Auto-generated method stub
 		recevoirtrends montrend= new recevoirtrends();
-		String[] monactu=montrend.prendretrends().split(" ");
+		String[] monactu=montrend.prendretrends();
 		for(String i :monactu){
 			System.out.println(i);
 		}
+		System.out.println(monactu.length);
 		
 	}
 
