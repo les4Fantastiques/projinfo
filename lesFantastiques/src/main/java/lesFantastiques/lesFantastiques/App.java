ab9229a6bc33ccbf785acf9c595bedb73974b69e
adri42 <adrien.moleins@telecom-st-etienne.fr>
2014-12-16 01:35:07 +0100

    fff

---
+++ lesFantastiques/src/main/java/lesFantastiques/lesFantastiques/App.java
@@ -0,0 +1,13 @@
+package lesFantastiques.lesFantastiques;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
